f9f369a97ba4e6beb989de6eb1158ce0c1af3f71
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2025-05-23 01:42:49 -0700

    commit

--- src/component/LandingPage.jsx
+++ src/component/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage () {
             <div className="mx- flex flex-col border-2 border-gray-200 justify-end  py-5 mx-auto bg-gray-50 w-96 h-screen">
             <div>
            <div className="flex flex-col justify-center mx-auto">
-            <h1 className="font-bold text-lg px-8 py-">Welcome to Propx</h1>
+            <h1 className="font-bold text-lg px-8 py-">Welcome to Popx</h1>
             <p className="text-gray-500 text-sm px-8 py-1">Lorem ipsum dolor sit amet. <br/> consectetur adipiscing elit</p>
             <Link to={'/register'}>
             <button className="w-80 flex my-2 mx-auto justify-center items-center text-white text-xs font-bold p-2 rounded bg-purple-700">Create Account</button>
